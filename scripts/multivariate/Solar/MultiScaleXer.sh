f0259860b6d0fabdd47220becc87c8f090a393cb
gaocodecell <1325261393@qq.com>
2026-06-10 22:54:21 +0800

    main

---
+++ scripts/multivariate/Solar/MultiScaleXer.sh
@@ -0,0 +1,88 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=MultiScaleXer
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/solar/ \
+  --data_path solar_AL.txt \
+  --model_id solar_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 4 \
+  --factor 3 \
+  --enc_in 137 \
+  --dec_in 137 \
+  --c_out 137 \
+  --des 'Exp' \
+  --d_ff 512 \
+  --batch_size 8 \
+  --itr 1
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/solar/ \
+  --data_path solar_AL.txt \
+  --model_id solar_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 3 \
+  --factor 3 \
+  --enc_in 137 \
+  --dec_in 137 \
+  --c_out 137 \
+  --des 'Exp' \
+  --batch_size 8 \
+  --itr 1
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/solar/ \
+  --data_path solar_AL.txt \
+  --model_id solar_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 4 \
+  --factor 3 \
+  --enc_in 137 \
+  --dec_in 137 \
+  --c_out 137 \
+  --des 'Exp' \
+  --batch_size 8 \
+  --itr 1
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/solar/ \
+  --data_path solar_AL.txt \
+  --model_id solar_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 3 \
+  --factor 3 \
+  --enc_in 137 \
+  --dec_in 137 \
+  --c_out 137 \
+  --des 'Exp' \
+  --batch_size 8 \
+  --itr 1
---
+++ scripts/multivariate/exchange_rate/MultiScaleXerWoGate.sh
@@ -0,0 +1,93 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=MultiScaleXer_woGate
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 256 \
+  --d_ff 512 \
+  --batch_size 8 \
+  --itr 1 \
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 3 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 1024 \
+  --batch_size 8 \
+  --itr 1
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 256 \
+  --batch_size 8 \
+  --itr 1
+
+python3 -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 128 \
+  --batch_size 8 \
+  --itr 1
